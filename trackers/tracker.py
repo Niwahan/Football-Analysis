3f015ddd74215bc168919741cc5a2c02e5e09726
Niwahan <aniwahang@gmail.com>
2024-09-09 11:42:32 +0545

    Ball Acqisition Tracking

--- trackers/tracker.py
+++ trackers/tracker.py
@@ -19,8 +19,8 @@ class Tracker:
         df_ball_positions = pd.DataFrame(ball_positions, columns=['x1','y1','x2','y2'])
         
         # Interpolate Missing Values
-        df_ball_positions = df_ball_positions.interpolate()
-        df_ball_positions = df_ball_positions.bfill()
+        df_ball_positions = df_ball_positions.interpolate(method='linear', limit_direction='both')
+        df_ball_positions = df_ball_positions.bfill().ffill()
         
         ball_positions = [{1: {"bbox":x}} for x in df_ball_positions.to_numpy().tolist()]
         
@@ -163,6 +163,9 @@ class Tracker:
             for track_id, player in player_dict.items():
                 color = player.get("team_color", (0,0,255))
                 frame = self.draw_ellipse(frame, player["bbox"], color, track_id)
+                
+                if player.get("has_ball", False):
+                    frame = self.draw_triangle(frame, player["bbox"], (0,0,255))
             
             # Draw Referees
             for _, referee in referee_dict.items():
